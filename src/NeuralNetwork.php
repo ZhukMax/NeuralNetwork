b828928094a0772e77943dc64ae2d81fb5038b27
Max Zhuk <zhukmax@ya.ru>
2018-01-21 21:44:09 +0300

    Update NeuralNetwork.php

--- src/NeuralNetwork.php
+++ src/NeuralNetwork.php
@@ -6,6 +6,7 @@ use Zhukmax\NeuralNetwork\Nodes\Node;
 /**
  * Class NeuralNetwork
  * @package Zhukmax\NeuralNetwork
+ * @todo Добавить возможность создавать более одного исходящего нейрона
  */
 class NeuralNetwork extends AbstractNeuralNetwork
 {
@@ -14,20 +15,25 @@ class NeuralNetwork extends AbstractNeuralNetwork
      * @return array
      * @throws Exception
      */
-    public function set(array $input) : array
+    public function getResult(array $input) : array
     {
-        $output[0] = $input;
+        $output = array();
 
-        if (isset($this->network)) {
-            foreach ($this->network as $layer => $nodes) {
-                foreach ($nodes as $n => $synapses) {
-                    $node = new Node();
-                    $node->synapses = $synapses;
-                    $output[$layer][$n] = $node->activation($output[$layer - 1]);
-                }
-            }
-        } else {
+        if (!isset($this->network)) {
             throw new Exception('Synapses of network is empty');
+        }
+
+        foreach ($input as $item => $value) {
+            $output[self::LAYER . 0][self::NODE . $item] = $value;
+        }
+
+        foreach ($this->network as $layer => $nodes) {
+            foreach ($nodes as $n => $synapses) {
+                $node = new Node();
+                $node->synapses = $synapses;
+                $beforeLayer = self::LAYER . (preg_replace('/[^0-9]/i', '', $layer) - 1);
+                $output[$layer][$n] = $node->activation($output[$beforeLayer]);
+            }
         }
 
         return $output;
@@ -40,24 +46,64 @@ class NeuralNetwork extends AbstractNeuralNetwork
      */
     public function training(array $input, array $ideal) : bool
     {
-        $layers = count($this->network);
-        $result = $this->set($input);
-        $error = $this->error($ideal, [$result[$layers - 1][0]]);
-        $delta[$layers - 1][0] = Activation::deltaOutput($result[$layers - 1][0]);
+        $weightChange = array();
+        $layers = count((array)$this->network);
+        $result = $this->getResult($input);
+        $error = $this->error($ideal, [
+            $result[self::LAYER . $layers][self::NODE . 0]
+        ]);
 
-        for ($i = $layers; $i >= 0; $i--) {
-            $nodesInLayer = count($this->network);
-            $delta[$i - 2][0] = Activation::delta(
-                $result[$i - 2][0], $this->network[$i - 1][0], $delta[$i - 1][0]
-            );
+        // Delta for output node
+        $delta[self::LAYER . $layers][self::NODE . 0] = Activation::deltaOutput(
+            $result[self::LAYER . $layers][self::NODE . 0]
+        );
 
-            $grad[$i - 1][0] = $delta[$i - 1][0] * $result[$i - 2][0];
+        for ($i = $layers - 1; $i > 0; $i--) {
+            $layer = self::LAYER . $i;
+            $nextLayerId = self::LAYER . ($i + 1);
+            $nextLayer = $this->network->$nextLayerId;
+
+            foreach ($this->network->$layer as $key => $node) {
+                $tmpDelta = array();
+                $synapses = array();
+
+                $synapseNum = preg_replace('/[^0-9]/i', '', $key);
+                foreach ($nextLayer as $nextKey => $nextNode) {
+                    $tmpDelta[] = $delta[$nextLayerId][$nextKey];
+                    $synapses[] = $nextNode[$synapseNum];
+                }
+
+                // Delta for nodes
+                $delta[$layer][$key] = Activation::delta(
+                    $result[$layer][$key],
+                    $synapses,
+                    $tmpDelta
+                );
+            }
         }
 
-        // Add one iteration step
-        static::$iteration++;
+        for ($i = $layers; $i > 0; $i--) {
+            $layer = self::LAYER . $i;
+            $prevLayerId = self::LAYER . ($i - 1);
+            $changes = count($weightChange);
 
-        return true;
+            foreach ($this->network->$layer as $key => $node) {
+                // Gradient for synapse
+                $grad = $delta[$layer][$key] * $result[$prevLayerId][$key];
+
+                // Weight change for synapses
+                foreach ($node as $synapseId) {
+                    $weightChange[$changes + 1][$layer][$key][$synapseId] = $this->e * $grad +
+                        $this->a * $weightChange[$changes][$layer][$key][$synapseId];
+                }
+            }
+        }
+        print_r($weightChange);
+
+//        // Add one iteration step
+//        static::$iteration++;
+
+//        return true;
     }
 
     /**
